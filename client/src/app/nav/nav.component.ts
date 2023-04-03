a02a6e872d6c43a2194b34b588af2608eca1d7a4
peter-monahan <peter.b.monahan@gmail.com>
2023-04-03 10:45:56 -0500

    basic register and login functionality added

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+  showDropdown = false;
+
+  constructor(public accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+        next: response => {
+            console.log(response);
+        },
+        error: error => console.log(error)
+    })
+  }
+
+  toggleDropDown() {
+    this.showDropdown = !this.showDropdown;
+
+    if(this.showDropdown) {
+
+    }
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+
+}
